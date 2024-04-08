6b4ec76e1e16a6ce06a4257206fa0073cd81a356
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-08 14:07:16 +0200

    tailwind

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <h1>HELLO React</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
            Alexis TISSIER
@@ -17,7 +17,6 @@ function App() {
         >
           Learn React
         </a>
-      </header>
     </div>
   );
 }
