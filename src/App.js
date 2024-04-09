6fae2dfb94f6e2b6ca97c13ffd13ad94a264c803
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-09 15:25:00 +0200

    layout 10

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './components/Home';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <Router>
       <div>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} /> {}
-        </Switch>
+        <Header /> {}
+        <Layout>
+          <Switch>
+            <Route exact path="/" component={Home} />
+          </Switch>
+        </Layout>
         <Footer />
       </div>
     </Router>
