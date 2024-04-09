48a390ee4052bc7bd6966b270633042862ff5b94
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-09 15:12:41 +0200

    ficher style

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Card from './components/Card';
 
+
+
 function App() {
   return (
     <div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
+
 const Card = ({ img, name, price }) => {
     return (
       <div className="Card" style={{ width: '18rem', }} >
-        <img src={img} className="card-img-top" alt={name} />
+        <img src={img} className="" alt={name} />
         <div className="card-body">
         <h2 className="card-title">{name}</h2>
         <p className="card-text">Prix: {price}€</p>
