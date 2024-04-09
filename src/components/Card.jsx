a877edcc36faeb8691b9ddfd069a9772fde41888
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-09 16:52:00 +0200

    reactboostrap on

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Card as BootstrapCard } from 'react-bootstrap';
 import '../assets/styles/Card.css';
 
 const Card = ({ img, name, price }) => {
-    return (
-      <div className="Card" style={{ width: '18rem', }} >
-        <img src={img} className="" alt={name} />
-        <div className="card-body">
-        <h2 className="card-title">{name}</h2>
-        <p className="card-text">Prix: {price}€</p>
-        </div>
-      </div>
-    );
-  }
-  
-  export default Card;
+  return (
+    <BootstrapCard style={{ width: '18rem' }}>
+      <BootstrapCard.Img variant="top" src={img} alt={name} />
+      <BootstrapCard.Body>
+        <BootstrapCard.Title>{name}</BootstrapCard.Title>
+        <BootstrapCard.Text>Prix: {price}€</BootstrapCard.Text>
+      </BootstrapCard.Body>
+    </BootstrapCard>
+  );
+}
+
+export default Card;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,36 +1,31 @@
-
-
 import React from 'react';
-import { Link } from 'react-router-dom'; 
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 import logo from '../assets/images/Logo.webp';
 import { Image } from 'react-bootstrap';
 import '../assets/styles/Header.css';
 
-
 function Header() {
   return (
     <header>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <Link className="navbar-brand " to="/"> {}
+      <Navbar expand="lg" bg="body-tertiary">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
             <Image src={logo} alt="Logo" className="logo" />
-          </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Accueil</Link> {}
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/about">A propos</Link> {}
-              </li>
-              {}
-            </ul>
-          </div>
-        </div>
-      </nav>
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarSupportedContent" />
+          <Navbar.Collapse id="navbarSupportedContent">
+            <Nav className="ml-auto">
+              <Nav.Item>
+                <Nav.Link as={Link} to="/">Accueil</Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link as={Link} to="/about">A propos</Nav.Link>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
   );
 }
