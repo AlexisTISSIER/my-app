df3912b29029b4cff7e890af8f8cf7be3dd8ad71
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-09 15:35:37 +0200

    Navlink  12

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,38 +1,43 @@
+
 import React from 'react';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Card from './components/Card';
+import { Container, Row, Col } from 'react-bootstrap';
+import Card from './Card'; 
+import { NavLink } from 'react-router-dom';
 
-
-
-function App() {
+const Home = () => {
   return (
-    <div>
-      <Header />
-      <main>
-        <section className="ListeCard" style={{ display: 'flex', justifyContent: 'space-around', paddingTop: '100px' }}>
-          <Card
-            price="3"
-            name="Tacos à l’unité"
-            img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-          />
-
-          <Card
-            price="12"
-            name="Enchiladas"
-            img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-          />
-
-          <Card
-            price="15"
-            name="Mole poblano"
-            img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-          />
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <NavLink to="/plat/tacos-a-l-unite"> {}
+            <Card
+              price="3"
+              name="Tacos à l’unité"
+              img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+            />
+          </NavLink>
+        </Col>
+        <Col>
+          <NavLink to="/plat/enchiladas"> {}
+            <Card
+              price="12"
+              name="Enchiladas"
+              img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+            />
+          </NavLink>
+        </Col>
+        <Col>
+          <NavLink to="/plat/mole-poblano"> {}
+            <Card
+              price="15"
+              name="Mole poblano"
+              img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+            />
+          </NavLink>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
-export default App;
+export default Home;
