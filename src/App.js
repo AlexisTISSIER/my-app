a18d6db7d1c1eb955ee96b1987c18b588d6da908
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-09 17:05:34 +0200

    dishdetails pages

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Layout from './components/Layout';
-import Dishdetails from './components/Dishdetails'; 
+import Dishdetails from './pages/Dishdetails'; 
 import NotFound from './components/NotFound';
 import About from './pages/About';
 
