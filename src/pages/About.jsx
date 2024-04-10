8c9897362cc536601df82d52036605d6ae530c51
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-10 11:13:47 +0200

    correction about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap'; 
-import { NavLink } from 'react-router-dom'; 
+
 const About = () => {
   return (
     <Container>
@@ -11,16 +11,20 @@ const About = () => {
           <p>
             Bienvenue dans notre restaurant mexicain, où l'authenticité et la saveur se rencontrent pour créer une
             expérience culinaire inoubliable. Plongez dans un voyage gustatif à travers le Mexique avec notre menu
-            varié, mettant en vedette des plats traditionnels préparés avec amour et expertise. Chaque bouchée est une
+            varié, mettant en vedette des plats traditionnels préparés avec amour et expertise.
+          </p>
+          <p>
+            Chaque bouchée est une
             explosion de saveurs, que ce soit avec nos tacos à l’unité, des enchiladas généreusement garnies ou notre
             mole poblano exquis. Nous croyons en l'utilisation d'ingrédients frais et de qualité pour créer des plats
-            qui vous transportent directement au cœur de la cuisine mexicaine. Que vous soyez un amateur de plats épicés
+            qui vous transportent directement au cœur de la cuisine mexicaine.
+          </p>
+          <p>
+            Que vous soyez un amateur de plats épicés
             ou que vous préfériez des saveurs plus douces, notre menu offre quelque chose pour satisfaire toutes les
             papilles gustatives. Venez nous rejoindre pour une expérience culinaire authentique et chaleureuse dans une
             ambiance conviviale et accueillante. ¡Buen provecho!
           </p>
-          {}
-          <NavLink to="/contact">Contactez-nous</NavLink>
         </Col>
       </Row>
     </Container>
