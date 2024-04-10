8ccaf7bd6e81ec4ddd5c049b909785596032ff6c
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-10 10:13:17 +0200

    corrction link home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import Card from '../components/Card';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -10,31 +10,31 @@ const Home = () => {
       <main>
       <Row>
         <Col>
-          <NavLink to="/plat/tacos-a-l-unite"> {}
+          <Link to="/plat/tacos-a-l-unite"> {}
             <Card
               price="3"
               name="Tacos à l’unité"
               img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
             />
-          </NavLink>
+          </Link>
         </Col>
         <Col>
-          <NavLink to="/plat/enchiladas"> {}
+          <Link to="/plat/enchiladas"> {}
             <Card
               price="12"
               name="Enchiladas"
               img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
             />
-          </NavLink>
+          </Link>
         </Col>
         <Col>
-          <NavLink to="/plat/mole-poblano"> {}
+          <Link to="/plat/mole-poblano"> {}
             <Card
               price="15"
               name="Mole poblano"
               img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
             />
-          </NavLink>
+          </Link>
         </Col>
       </Row>
       </main>
