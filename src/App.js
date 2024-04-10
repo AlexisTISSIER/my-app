1e9f855c04ffbd6c630296323e4648a0767c217c
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-10 10:04:36 +0200

    importation correct de routes et route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Home from './pages/Home';
-import Layout from './components/Layout';
-import Dishdetails from './pages/Dishdetails'; 
-import NotFound from './components/NotFound';
-import About from './pages/About';
+import { Route, Routes } from "react-router-dom";
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import Home from "./pages/Home.jsx";
+import Layout from './components/Layout.jsx';
+import Dishdetails from './pages/Dishdetails.jsx'; 
+import NotFound from './components/NotFound.jsx';
+import About from './pages/About.jsx';
 
 function App() {
   return (
-    <Router>
       <div>
         <Header />
         <Layout>
-          <Route index path="/" component={Home} />
-          <Route path="/plat/:slug" element={<Dishdetails/>}/>
-          <Route path="/about" component={About} />
-          <Route component={NotFound} />
+          <Routes>
+            <Route index path="/" component={Home} />
+            <Route path="/plat/:slug" element={<Dishdetails/>}/>
+            <Route path="/about" component={About} />
+            <Route component={NotFound} />
+          </Routes>
         </Layout>
         <Footer />
       </div>
-    </Router>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -16,9 +16,11 @@ ReactDOM.render(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+  <BrowserRouter>
+  <App />
+  </BrowserRouter>
   </React.StrictMode>
-);
+  );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
