6bdc0be28bca709271d269585ac5ff6cf6254ffd
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-10 10:26:39 +0200

    retir import footer header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import Footer from './components/Footer';
 import Home from './pages/Home';
-import Layout from './components/Layout';
 import Dishdetails from './pages/Dishdetails'; 
 import NotFound from './components/NotFound';
 import About from './pages/About';
