5afae04c8ea5da5e8e6897e4bac8a9b12d144247
AlexisTISSIER <134628532+AlexisTISSIER@users.noreply.github.com>
2024-04-10 13:38:57 +0200

    dish tab 15

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,42 @@ import { Container, Row, Col } from 'react-bootstrap';
 import Card from '../components/Card';
 
 const Home = () => {
+  const dishes = [
+    {
+      slug: "/plat/tacos-a-l-unite",
+      price: "3",
+      name: "Tacos à l’unité",
+      img: "https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
+    },
+    {
+      slug: "/plat/enchiladas",
+      price: "12",
+      name: "Enchiladas",
+      img: "https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
+    },
+    {
+      slug: "/plat/mole-poblano",
+      price: "15",
+      name: "Mole poblano",
+      img: "https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
+    }
+  ];
+
   return (
     <div>
       <Container>
         <Row>
-          <Col>
-            <Card
-              slug="/plat/tacos-a-l-unite"
-              price="3"
-              name="Tacos à l’unité"
-              img="https://cdn.pixabay.com/photo/2016/08/23/08/53/tacos-1613795_960_720.jpg"
-            />
-          </Col>
-          <Col>
-            <Card
-              slug="/plat/enchiladas"
-              price="12"
-              name="Enchiladas"
-              img="https://cdn.pixabay.com/photo/2014/01/14/22/13/mexican-245240_960_720.jpg"
-            />
-          </Col>
-          <Col>
-            <Card
-              slug="/plat/mole-poblano"
-              price="15"
-              name="Mole poblano"
-              img="https://cdn.pixabay.com/photo/2021/02/04/03/57/mole-5980185_960_720.jpg"
-            />
-          </Col>
+          {}
+          {dishes.map((dish) => (
+            <Col key={dish.slug}>
+              <Card
+                slug={dish.slug}
+                price={dish.price}
+                name={dish.name}
+                img={dish.img}
+              />
+            </Col>
+          ))}
         </Row>
       </Container>
     </div>
